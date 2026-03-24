c098d50a596a64056e37fe2b4bbf2efc3d1c135b
Даниил Калашников <ddkalashnikov@edu.hse.ru>
2026-03-24 16:49:42 +0300

    synchronizer: попытка 3 ( он почему то ругается на BuoundedBlockingQueue ) - поэтому я добавил из предыдущей попоытки

--- src/main/java/hse/java/lectures/lecture6/tasks/queue/BoundedBlockingQueue.java
+++ src/main/java/hse/java/lectures/lecture6/tasks/queue/BoundedBlockingQueue.java
@@ -1,25 +1,56 @@
 package hse.java.lectures.lecture6.tasks.queue;
 
+import java.util.LinkedList;
+
 public class BoundedBlockingQueue<T> {
+    final Integer capacity ;
+    LinkedList<T> list;
+    Object monitor ;
 
 
     public BoundedBlockingQueue(int capacity) {
+        if ( capacity <= 0 ) {
+            throw new IllegalArgumentException() ;
+        }
+        this.capacity = capacity;
+        list = new LinkedList<>();
+        monitor = new Object();
+
 
     }
 
-    public void put(T item) {
+    public void put(T item) throws InterruptedException {
+        if ( item == null) throw new IllegalArgumentException() ;
+
+
+        synchronized (monitor) {
+            while ( capacity ==  list.size()) {
+                monitor.wait();
+            }
+            list.add(item) ;
+            monitor.notifyAll();
+        }
 
     }
 
-    public T take() {
-        return null;
+    public T take() throws InterruptedException {
+        synchronized (monitor) {
+            while (list.isEmpty()) {
+                monitor.wait();
+            }
+            T res  = list.removeFirst();
+            monitor.notifyAll();
+            return  res;
+        }
     }
 
     public int size() {
-        return 0;
+        synchronized (monitor) {
+            return list.size();
+        }
     }
 
     public int capacity() {
-        return 0;
+        return capacity;
     }
 }
